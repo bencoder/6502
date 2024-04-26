125eb3ce8b3fd97f79b965d8768c5d2aac30e643
Ben <bencoder@gmail.com>
2024-04-26 19:05:58 +0100

    Speed up the tests significantly by not outputting when unecessary, use some variables to only output when PC is at the part we care about

--- test/test.ts
+++ test/test.ts
@@ -8,22 +8,23 @@ const ram = new TestRam(ramData);
 const processor = new Processor(ram);
 processor.setPC(0x0400);
 
-const startTime = Date.now();
-let cyclesSinceStart = 0;
-let hz = 1000000;
-const interval = setInterval(() => {
-  const secondsElapsed = (Date.now() - startTime) / 1000;
-  const totalExpectedCycles = secondsElapsed * hz;
-  while (cyclesSinceStart < totalExpectedCycles) {
-    const pcBefore = processor.getPC();
-    cyclesSinceStart += processor.tick(true);
-    if (processor.getPC() === pcBefore) {
-      console.log("TRAPPED");
-      const stack = ramData.subarray(0x100, 0x200);
-      console.log(stack.toString("hex").match(/../g)?.join(" "));
-      process.exit();
-    }
-  }
-}, 1);
+//output debug data when the PC is between these values
+const traceStart = 0x346f;
+const traceEnd = 0x3500;
 
-process.on("beforeExit", () => {});
+let cycles = 0;
+const start = Date.now();
+while (true) {
+  const pcBefore = processor.getPC();
+  cycles += processor.tick(pcBefore >= traceStart && pcBefore < traceEnd);
+  if (processor.getPC() === pcBefore) {
+    const timeSpent = Date.now() - start;
+    const equivalentMhz = Math.floor(cycles / (timeSpent / 1000) / 1000) / 1000;
+    console.log(
+      `TRAPPED at ${pcBefore.toString(16)} after ${cycles} cycles and ${timeSpent}ms (${equivalentMhz}Mhz)`
+    );
+    const stack = ramData.subarray(0x100, 0x200);
+    console.log(stack.toString("hex").match(/../g)?.join(" "));
+    process.exit();
+  }
+}
